a2f65b00a47042b59b28964b1876829bd7783ed3
sunsy <a120100303@abv>
2022-08-30 18:04:18 +0800

    doc: 工作原理

---
+++ Singleton/src/main/java/com/example/singleton/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.singleton.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * SpringSecurity安全配置类
+ *  WebSecurityConfigurerAdapter是对WebSecurityConfigurer一个实现
+ *  实现了默认的认证和授权, 允许用户自定义一个WebSecurity类，重写其中的三个configure来实现自定义的认证和授权
+ */
+@Configuration // 由于spring boot的自动装配, 不需要@EnableWebSecurity, @WebSecurityConfig
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
